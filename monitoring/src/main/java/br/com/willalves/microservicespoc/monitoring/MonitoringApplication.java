cfd5254ff9d4087f356e260c607f48201c77480e
will <william@exactaworks.com.br>
2019-04-24 14:41:27 -0300

    little fixes

--- monitoring/src/main/java/br/com/willalves/microservicespoc/monitoring/MonitoringApplication.java
+++ monitoring/src/main/java/br/com/willalves/microservicespoc/monitoring/MonitoringApplication.java
@@ -2,8 +2,10 @@ package br.com.willalves.microservicespoc.monitoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableHystrixDashboard
 public class MonitoringApplication {
 
 	public static void main(String[] args) {
